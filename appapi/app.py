8a7ccb5c2e00953bd8cf265d66efe7f8517af3fb
Charubaiel <aleksandrin.a@mail.ru>
2022-03-02 23:24:54 +0300

    api

---
+++ appapi/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from appapi.ml import *
+app = FastAPI()
+
+
+
+class Item(BaseModel):
+    comment: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/is_toxic")
+def create_item(item: Item):
+    return f'"{item.comment}" токсично на {voting(str(item.comment))[0]:.1%}'
